41c2dd9b051059dab146712cb84141dd38c7e1bb
Andrey Popp <8mayday@gmail.com>
2017-05-29 19:09:33 +0300

    Expand variables
    We now expand some of the vars, need to get a list of all opam vars from
    somewhere.

---
+++ src/EsyOpamRenderer.ml
@@ -0,0 +1,82 @@
+type result = {
+  dependencies: (OpamTypes.name * string) list;
+  build: string list;
+}
+
+let render_relop (op : OpamParserTypes.relop) =
+  match op with
+  | `Leq -> " <= "
+  | `Lt -> " < "
+  | `Neq -> "!="
+  | `Eq -> "="
+  | `Geq -> " >= "
+  | `Gt -> " > "
+
+let rec list_of_formula_atoms depends =
+  match depends with
+  | OpamFormula.Empty -> []
+  | OpamFormula.Atom item -> [item]
+  | OpamFormula.Block f -> list_of_formula_atoms f
+  | OpamFormula.And (a, b) -> List.append (list_of_formula_atoms a) (list_of_formula_atoms b)
+  | OpamFormula.Or (a, b) -> List.append (list_of_formula_atoms a) (list_of_formula_atoms b)
+
+let rec list_of_filter_atoms (filter: OpamTypes.filter) =
+  match filter with
+  | OpamTypes.FBool _ -> ""
+  | OpamTypes.FString s -> s
+  | OpamTypes.FIdent _ -> ""
+  | OpamTypes.FOp (a, op, b) -> (list_of_filter_atoms a) ^ (render_relop op) ^ (list_of_filter_atoms b)
+  | OpamTypes.FAnd (a, b) -> (list_of_filter_atoms a) ^ " " ^ (list_of_filter_atoms b)
+  | OpamTypes.FOr (a, b) -> (list_of_filter_atoms a) ^ " || " ^ (list_of_filter_atoms b)
+  | OpamTypes.FNot _ -> ""
+  | OpamTypes.FDefined _ -> ""
+  | OpamTypes.FUndef _ -> ""
+
+let render_version_constraint filter =
+  let filter = list_of_formula_atoms filter in
+  List.map
+    (fun f ->
+       match f with
+       | OpamTypes.Filter f ->
+         list_of_filter_atoms f
+       | OpamTypes.Constraint (op, f) ->
+         (render_relop op) ^ (list_of_filter_atoms f))
+    filter
+
+let render_opam_depends depends =
+  let depends = list_of_formula_atoms depends in
+  let dependencies = List.map (fun (name, constr) ->
+      let constr = render_version_constraint constr in
+      let constr = String.concat " " constr in
+      [name, if constr == "" then "*" else constr]) depends in
+  List.concat dependencies
+
+let render_opam_build env (commands: OpamTypes.command list) =
+  let render_args args =
+    let args = List.map
+        (fun (arg, _filter) -> match arg with
+           | OpamTypes.CString arg -> OpamFilter.expand_string env arg
+           | OpamTypes.CIdent name -> name)
+        args
+    in String.concat " " args
+  in
+  List.map
+    (fun (args, _filter) -> render_args args)
+    commands
+
+let render_opam opam =
+  let env (var: OpamVariable.Full.t) =
+    let variable = OpamVariable.Full.variable var in
+    let name = OpamVariable.to_string variable in
+    let yes = Some (OpamVariable.B true) in
+    match name with
+    | "installed" -> yes
+    | "ocaml-native" -> yes
+    | "ocaml-native-dynlink" -> yes
+    | _ -> None
+  in
+  let
+    dependencies = render_opam_depends (OpamFile.OPAM.depends opam) and
+    build = render_opam_build env (OpamFile.OPAM.build opam)
+  in
+  { dependencies = dependencies; build = build }
