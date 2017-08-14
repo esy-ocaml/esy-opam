a042444ce8429a1a08140dfb1d5479725288857f
Andrey Popp <8mayday@gmail.com>
2017-08-14 12:04:19 +0300

    Fix test script

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-var EsyOpam = require('./bundle.js');
+var EsyOpam = require('./index.js');
 console.log(EsyOpam);
 var fs = require('fs');
 
 var data = fs.readFileSync(process.argv[2], 'utf8');
 var opam = EsyOpam.parseOpam(data);
-var res = EsyOpam.renderOpamToJs('package', opam);
+var res = EsyOpam.renderOpam('package', '0.1.1', opam);
 
 console.log(res);
